ac173aa1393d867fc05b8bd7251effdbb68edfd9
Aflitos, Saulo Alves <sauloalves.aflitos@wur.nl>
2015-02-26 04:03:43 +0100

    docker

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,14 @@
 touch access.log error.log
-docker run -it --security-opt="apparmor:unconfine" \
--v $PWD/data:/var/www/ibrowser/data \
+
+#--security-opt="apparmor:unconfine" \
+
+docker run -it --rm \
+-v $PWD/..:/var/www/ibrowser \
+-v $PWD/../data:/var/www/ibrowser/data \
 -v $PWD/access.log:/var/log/apache2/access.log \
 -v $PWD/error.log:/var/log/apache2/error.log \
+-p 0.0.0.0:9000:10000 \
 --hostname="assembly.ab.wurnet.nl" \
-ibrowser
+--name ibrowser \
+sauloal/ibrowser
 

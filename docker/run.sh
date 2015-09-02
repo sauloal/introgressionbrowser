26475eae10df6d297061e3668a364180226d87f5
Saulo <sauloal@gmail.com>
2015-09-02 10:22:58 +0200

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -5,10 +5,7 @@ touch access.log error.log
 docker run -it --rm \
 -v $PWD/..:/var/www/ibrowser \
 -v $PWD/../data:/var/www/ibrowser/data \
--v $PWD/access.log:/var/log/apache2/access.log \
--v $PWD/error.log:/var/log/apache2/error.log \
--p 0.0.0.0:9000:10000 \
---hostname="assembly.ab.wurnet.nl" \
+-p 0.0.0.0:10000:10000 \
 --name ibrowser \
 sauloal/introgressionbrowser
 
